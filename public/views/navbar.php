08ff763ab8124acb25f6fbd6748ac8a8c2c91c6b
Kupsztajn <nawrocki.jakub@gmail.com>
2025-11-18 16:51:38 +0100

    feat: added navbar

---
+++ public/views/navbar.php
@@ -0,0 +1,9 @@
+<nav class="navbar">
+    <div class="nav-left">
+        <a href="/" class="nav-btn">🏠 Home</a>
+        <a href="/dashboard" class="nav-btn">📊 Dashboard</a>
+    </div>
+    <div class="nav-right">
+        <a href="/logout" class="nav-btn logout">🚪 Logout</a>
+    </div>
+</nav>
